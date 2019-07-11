eada022422e3878dca04c446ea9fa98453031b91
Justin Kahn <justindavidkahn@gmail.com>
2019-07-11 17:31:04 +0800

    Initialize Project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Validate
+
+@alifd/validate
+
+Validation to be used with @alifd/field
+
+## API
+
+| 参数名 | 说明 | 必填 | 类型 | 默认值 | 备注 |
+| ------ | ---- | ---- | ---- | ------ | ---- |
+|        |      |      |      |        |      |
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Validate extends Component {
+  static displayName = 'Validate';
+
+  render() {
+    return (
+      <div className="validate">Hello Validate</div>
+    );
+  }
+}
