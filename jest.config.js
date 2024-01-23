b2fd1581816ae935d8cb08a659b8a97d2e28ec0e
珵之 <chengzhi.zpc@alibaba-inc.com>
2024-01-23 15:57:33 +0800

    chore: refactor build tools for ts project

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     coverageDirectory: './coverage/',
     collectCoverage: true,
     verbose: true,
+    preset: 'ts-jest/presets/js-with-ts',
 };
